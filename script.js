8c014c39cdc4ee41c9bdc5c60038dc41f2b88175
Szikras <bodnar.4440@gmail.com>
2025-03-10 18:11:02 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ document.addEventListener("DOMContentLoaded", function () {
     var variantSelect = document.getElementById("egyeb_list1_KT_airedale_terrier_b2_112_Teszt");
 
     // A szövegbeviteli mező elemének elérése
-    var szovegMezo = document.getElementById("szoveg_mezo");
+    var szovegMezo = document.getElementById("page_artdet_product_param_spec_4696445");
 
     if (variantSelect && szovegMezo) {
         // Eseménykezelő a legördülő menü változásaira
